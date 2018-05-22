b7629de834f8a8d5434c2cbf92675ce8990265ea
QUAN THE TRAN <qtran88@cs.washington.edu>
2018-05-22 13:33:48 -0700

    fixed the sum, the partial not works yet

--- lastDigitOfPartial_SumFibonnaci.py
+++ lastDigitOfPartial_SumFibonnaci.py
@@ -1,41 +1,64 @@
-#python 3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun May 20 18:19:04 2018
-
-@author: Quan_Linh
-"""
-
+# Uses python3
 # -*- coding: utf-8 -*-
 """
 Created on Sun May 20 17:51:46 2018
 
 @author: Quan_Linh
-"""
+formula for 
+F0 + F1 + F2 + ... _ Fn
+sum = F(n+2) - F(2)
+sum = F(n+2) - 1
+The key here to using pisano period for % 10
+will be 60. After that, the formula for the 
+Fibonacci term can be calculate with the 
+proved formula above at 
+https://www.quora.com/What-is-the-sum-of-n-terms-of-a-Fibonacci-series
+Finally, the last two term need to be taken for the 
+edge cases that the last value is 0 - 1 would give it 
+a negative. So only the first term should take into account in this cases.
 
-def lastDigitsOfSumFibonnacci(fibonacciTermBegin,fibonacciTermBeginEnd,m=10):    
-    if fibonacciTermBeginEnd == 0:
+F(n+2) - 1 - [F(m+2)-1] = F(n+2)-F(m+2)
+"""
+import math
+def fibonannciModuloM(fibonacciTerm,m=10):    
+    if fibonacciTerm == 0:
         return 0
-    elif fibonacciTermBeginEnd == 1:
+    elif fibonacciTerm == 1:
         return 1
     else:
         first = 0
         second = 1
-        sumOfMod10 = 0
-        for i in range(fibonacciTermBeginEnd-1):
+        for i in range(fibonacciTerm-1):
             temp = first
             first = second
             second = (temp+second)%m
-            # minus 2 because two first tem had been handle
-            if i >= fibonacciTermBegin-2: 
-             #   print(i,"i")
-                sumOfMod10+=second            
-            if  sumOfMod10 >=10:
-                sumOfMod10 %= 10
-            #print('-',sumOfMod10,end = '/')
-            #print(second,end=' ')
-        #print("---")    
-        return sumOfMod10
+        return second
 if __name__ == "__main__":
-    beginFibonacci,endFibonacci = map(int,input().split())
-    print(lastDigitsOfSumFibonnacci(beginFibonacci,endFibonacci))
+    lengthOfMod10Period = 60
+    answer = None
+    m,n = map(int,input().split())       
+    if n+2 < lengthOfMod10Period:
+        if m==n:
+            answer = fibonannciModuloM(n)
+        else:
+            firstAnswer  =  fibonannciModuloM(n+2)
+            secondAnswer = fibonannciModuloM(m+1)
+            answer = firstAnswer-secondAnswer
+            print(firstAnswer)
+            print(secondAnswer)
+    else:
+        if m==n:
+            simpleFibonanciFirst  =  n % lengthOfMod10Period
+            answer = fibonannciModuloM(simpleFibonanciFirst)
+        else:            
+            simpleFibonanciFirst  =  (n+2) % lengthOfMod10Period
+            simpleFibonanciSecond =  (m+1) % lengthOfMod10Period
+            firstAnswer  =   fibonannciModuloM(simpleFibonanciFirst)
+            secondAnswer =   fibonannciModuloM(simpleFibonanciSecond)
+            answer = firstAnswer-secondAnswer
+    if answer < 0:
+        print(firstAnswer) 
+        print(secondAnswer)
+    else:
+        print(answer)
+ 
--- lastDigitOfSumFibonnaci.py
+++ lastDigitOfSumFibonnaci.py
@@ -4,28 +4,43 @@
 Created on Sun May 20 17:51:46 2018
 
 @author: Quan_Linh
+formula for 
+F0 + F1 + F2 + ... _ Fn
+sum = F(n+2) - F(2)
+sum = F(n+2) - 1
+The key here to using pisano period for % 10
+will be 60. After that, the formula for the 
+Fibonacci term can be calculate with the 
+proved formula above at 
+https://www.quora.com/What-is-the-sum-of-n-terms-of-a-Fibonacci-series
+Finally, the last two term need to be taken for the 
+edge cases that the last value is 0 - 1 would give it 
+a negative. So only the first term should take into account in this cases.
 """
-
-def lastDigitsOfSumFibonnacci(fibonacciTerm,m=10):    
+def fibonannciModuloM(fibonacciTerm,m=10):    
     if fibonacciTerm == 0:
-        return 0
+        return None,0
     elif fibonacciTerm == 1:
-        return 1
+        return 0,1
     else:
         first = 0
         second = 1
-        sumOfMod10 = 1
         for i in range(fibonacciTerm-1):
             temp = first
             first = second
             second = (temp+second)%m
-            sumOfMod10+=second
-            if sumOfMod10 >=10:
-                sumOfMod10 %= 10
-            #print(sumOfMod10,end = '/')
-            #print(second,end=' ')
-            
-        return sumOfMod10
+        return first,second
 if __name__ == "__main__":
-    n = int(input())
-    print(lastDigitsOfSumFibonnacci(n))
+    lengthOfMod10Period = 60
+    answer = None
+    n = int(input())   
+    if n+2 < lengthOfMod10Period:
+        first,second = fibonannciModuloM(n+2)
+    else:
+        simpleFibonanci = (n+2) % lengthOfMod10Period
+        first,second = fibonannciModuloM(simpleFibonanci)
+    if second-1 < 0 and n != 0:
+        print(first)
+    else:        
+        print(second-1)
+ 
